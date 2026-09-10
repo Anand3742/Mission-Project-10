ad7e543188cab9d5206f1ca8af39c07699afe856
Anand3742 <111744805+Anand3742@users.noreply.github.com>
2026-09-10 12:00:09 +0530

    Update GymManagementDAOInt.java

--- ORSProject-10/src/main/java/com/rays/dao/GymManagementDAOInt.java
+++ ORSProject-10/src/main/java/com/rays/dao/GymManagementDAOInt.java
@@ -3,6 +3,6 @@ package com.rays.dao;
 import com.rays.common.BaseDAOInt;
 import com.rays.dto.GymManagementDTO;
 
-public interface GymManagementDAOInt extends BaseDAOInt<GymManagementDTO>{
+public interface GymManagementDAOInt extends BaseDAOInt<GymManagementDTO>{ 
 
 }
